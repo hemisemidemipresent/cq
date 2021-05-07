60751d0f08ac921298d95d6962033500ce0c9b92
hnngggrrrr <57880110+hnngggrrrr@users.noreply.github.com>
2021-05-07 22:26:00 +0800

    try again pt 2

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -2,7 +2,7 @@
 module.exports = {
     title: 'My Site',
     tagline: 'Dinosaurs are cool',
-    url: 'https://your-docusaurus-test-site.com',
+    url: 'https://cq.netlify.app',
     baseUrl: '/docs/build/',
     onBrokenLinks: 'throw',
     onBrokenMarkdownLinks: 'warn',
